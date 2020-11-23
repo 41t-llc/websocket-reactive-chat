310f4442b9ca89d370af864409b676eec7c11b5d
Ishi Inanis <ishi.inanis@gmail.com>
2020-11-24 02:26:16 +0300

    fix responsive redesign

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,93 +1,113 @@
-let HOST = "ws://localhost:3000";//location.origin.replace(/^http/, 'ws'),
+let HOST = location.origin.replace(/^http/, 'ws');
     ws = new WebSocket(HOST),
     chat = document.querySelector("#chat"),
     condition = document.querySelector("#condition"),
+    indicator = document.querySelector("#indicator"),
+    sysmes = document.querySelector("#sysmes"),
+    forms = document.forms;
     user = "",
-    users = [
-      {
-      login: "user1",
-      password: "111"
-      },
-      {
-      login: "user2",
-      password: "222"
-      },
-      {
-      login: "user3",
-      password: "333"
-      },
-      {
-      login: "user4",
-      password: "444"
-      }
-    ];
+    users = [{ // Временно
+        login: "user1",
+        password: "111"
+      }, {
+        login: "user2",
+        password: "222"
+      }, {
+        login: "user3",
+        password: "333"
+      }, {
+        login: "user4",
+        password: "444"
+    }];
 ws.onopen = function() {
-  condition.querySelector("#indicator").classList.add("success");
-  condition.querySelector("#indicator").classList.remove("error");
-  condition.querySelector("#sysmes").innerHTML = "Соединение установлено";
+  indicator.classList.add("success");
+  indicator.classList.remove("error");
+  sysmes.innerHTML = "Соединение установлено";
 };
 
 ws.onclose = function(event) {
-  condition.querySelector("#indicator").classList.add("error");
-  condition.querySelector("#indicator").classList.remove("success");
+  indicator.classList.add("error");
+  indicator.classList.remove("success");
 
   if (event.wasClean) {
-      condition.querySelector("#sysmes").innerHTML = "Соединение закрыто: <br>";
+    sysmes.innerHTML = "Соединение закрыто: <br>";
   } else {
-      condition.querySelector("#sysmes").innerHTML = "Соединения как-то закрыто <br>";
+    sysmes.innerHTML = "Соединения как-то закрыто <br>";
   }
-    condition.querySelector("#sysmes").innerHTML += `<i>код: ${event.code} причина: ${event.reason}</i>`;
+  // TODO: Нужно ли перенести в условие?
+  sysmes.innerHTML += `<i>код: ${event.code} причина: ${event.reason}</i>`;
 };
 
 ws.onmessage = function(event) {
   let message = JSON.parse(event.data),
   children = chat.children;
 
-  if(children.length > 0) {
-    lastChild = children[(children.length - 1)]
-    if(lastChild.children[0].innerText == message.user) {
-        lastChild.children[1].innerHTML += `<article class="message grid gap-15"> <i>${message.date}</i><span>${message.text}<span></article>`;
-      } else{
-        chat.innerHTML += `<article class="user_message mymsgs"><p>${message.user}</p><article class="items grid gap"><article class="message grid gap-15"> <i>${message.date}</i><span>${message.text}</article></article>`;
-      }
+  if (children.length > 0) {
+    lastChild = children[children.length - 1]
+    if (lastChild.children[0].innerText == message.user) {
+      lastChild.children[1].innerHTML += `
+          <article class="message flex ai-c grid gap-15">
+            <i>${message.date}</i>
+            <span>${message.text}</span>
+          </article>`;
+    } else {
+      chat.innerHTML += `
+          <article class="user_message mymsgs">
+            <p class="m">${message.user}</p>
+            <section class="items grid gap">
+              <article class="message flex ai-c grid gap-15">
+                <i>${message.date}</i>
+                <span>${message.text}</span>
+              </article>
+            </section>
+          </article>`;
+    }
   } else {
-    chat.innerHTML += `<article class="user_message mymsgs"><p>${message.user}</p><article class="items grid gap"><article class="message grid gap-15"> <i>${message.date}</i><span>${message.text}</article></article>`;
+    chat.innerHTML += `
+        <article class="user_message mymsgs">
+          <p class="m">${message.user}</p>
+          <section class="items grid gap">
+            <article class="message flex ai-c grid gap-15">
+              <i>${message.date}</i>
+              <span>${message.text}</span>
+            </article>
+          </section>
+        </article>`;
   }
-
-  document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
+  chat.scrollTop = chat.scrollHeight;
 };
 
 ws.onerror = function(event) {
-  condition.querySelector("#indicator").classList.add("error");
-  condition.querySelector("#indicator").classList.remove("success");
-  condition.querySelector("#indicator").innerHTML = `<b>Система:</b>ошибка ${event.message}`;
+  indicator.classList.add("error");
+  indicator.classList.remove("success");
+  sysmes.innerHTML = `<b>Система:</b>ошибка ${event.message}`;
 };
 
-document.forms["message"].onsubmit = function() {
+forms["message"].onsubmit = function() {
   if (this.text.value != ""){
     let message = {
-          user: user,
-          text: this.text.value,
-          date: new Date().toLocaleTimeString().substring(0,5)
-        };
-        this.text.value = "";
+      user: user,
+      text: this.text.value,
+      date: new Date().toLocaleTimeString().substring(0, 5)
+    };
+    this.text.value = "";
     ws.send(JSON.stringify(message));
 
   } else {
     this.text.focus();
-
   }
-     return false;
+  return false;
 }
 
-document.forms["signin"].onsubmit = function () {
+forms["signin"].onsubmit = function () {
   if (this.login.value != "" && this.password.value != "") {
     for (i = 0; i < users.length; i++) {
-      if (this.login.value == users[i].login && this.password.value == users[i].password) {
+      if (this.login.value == users[i].login &&
+          this.password.value == users[i].password) {
         user = this.login.value;
-        document.forms["message"].querySelector("#user_name").innerHTML = `<b>${user}</b>`;
+        forms["message"].querySelector("#user_name").innerHTML = `<b>${user}</b>`;
         this.style.display = "none";
-        document.forms["message"].style.display = "grid";
+        forms["message"].style.display = "grid";
         chat.style.display = 'block';
         break;
       }
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const INDEX = '/views/index.html';
 
 const server = express()
+  .use("/public", express.static(path.join(__dirname, '/public')))
   .use((req, res) => res.sendFile(INDEX, {root: __dirname}))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
